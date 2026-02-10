139029f91c400d97f82e142e4ad0f44004b06a09
SnehaNair3 <snehanairk369@gmail.com>
2026-02-10 18:16:24 +0530

    movie- CRUD operation - add movie added.

---
+++ movieApi/src/main/java/com/movieflix/movieApi/controllers/MovieController.java
@@ -0,0 +1,41 @@
+package com.movieflix.movieApi.controllers;
+
+
+import com.movieflix.movieApi.dto.MovieDto;
+import com.movieflix.movieApi.service.MovieService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import tools.jackson.databind.ObjectMapper;
+
+@RestController
+@RequestMapping("/api/v1/movie")
+public class MovieController {
+
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    //adding movie to DB
+    @PostMapping("/add-movie")
+    public ResponseEntity<MovieDto> addMovieHandler(@RequestPart MultipartFile file,
+                                                    @RequestPart String movieDto){
+
+
+    }
+
+    private MovieDto convertToMovieDto(String movieDtoObj){
+
+
+         //spring provides an object mapper
+        ObjectMapper objectMapper=new ObjectMapper();
+
+
+        return objectMapper.readValue(movieDtoObj,MovieDto.class);
+    }
+}
